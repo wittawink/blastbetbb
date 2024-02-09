76434567131841010c33195b30e9764f28547481
Wittawin Kahuttanaseth <wittawin.k@gmail.com>
2024-02-10 00:57:47 +0700

    implement BaseProfile and LayoutFooter

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,10 +1,12 @@
 import BaseButton from "@/ui/base/BaseButton";
+import LayoutFooter from "@/ui/layout/LayoutFooter";
 
 export default function Home() {
   return (
-    <main className="flex items-center p-24">
+    <main className="p-24">
       <div className="text-7xl">Hello CryptDegree</div>
       <BaseButton text={"Hello Button"}></BaseButton>
+      <LayoutFooter className="mt-[100px]" />
     </main>
   );
 }
--- src/ui/base/BaseProfile/index.tsx
+++ src/ui/base/BaseProfile/index.tsx
@@ -1,9 +1,19 @@
+import cn from "@/lib/cn";
 import React from "react";
 
 interface BaseProfileProps {
-  text: string;
+  className?: string;
+  profileName: string;
 }
 
-export default function BaseProfile({ text }: BaseProfileProps) {
-  return <div className="">{text}</div>;
+export default function BaseProfile({
+  className,
+  profileName,
+}: BaseProfileProps) {
+  return (
+    <div className={cn(className, "flex flex-row items-center")}>
+      <div className="w-[90px] h-[90px] bg-[#FCFC03] rounded-full"></div>
+      <div className="ml-[24px] text-[36px]">{profileName}</div>
+    </div>
+  );
 }
