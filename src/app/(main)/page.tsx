ffcc3d247271c81a286a98b2c2b8dc472d780ed9
popohatari <ajubuwoohoo@gmail.com>
2024-02-10 00:49:21 +0700

    add connect wallet button

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,8 +3,7 @@ import BaseButton from "@/ui/base/BaseButton";
 export default function Home() {
   return (
     <main className="flex items-center p-24">
-      <div className="text-7xl">Hello CryptDegree</div>
-      <BaseButton text={"Hello Button"}></BaseButton>
+      <BaseButton text={"Connect Wallet"}></BaseButton>
     </main>
   );
 }
