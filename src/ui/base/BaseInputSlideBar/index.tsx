98e98fc6b87ab8af6dbc7b624cea74b425a35e4f
popohatari <ajubuwoohoo@gmail.com>
2024-02-13 15:33:53 +0700

    fix button and slide bar

--- src/ui/base/BaseInputSlideBar/index.tsx
+++ src/ui/base/BaseInputSlideBar/index.tsx
@@ -9,9 +9,11 @@ interface BaseInputSlideBarProps {
 export default function BaseInputSlideBar({ text }: BaseInputSlideBarProps) {
   return (
     <div>
-      <p className="text-[20px] font-semibold">Wager</p>
-      <div className="flex items-center mt-[8px] w-[408px] h-[73px] border-2 border-[#FCFC03]">
-        <p className="text-base font-semibold ml-[18px]">Choose a wager...</p>
+      <p className="text-[20px] font-bold text-[#FCFDC7]">{text}</p>
+      <div className="flex items-center mt-[8px] w-[408px] h-[73px] bg-[#11140C] border-2 rounded-[10px] border-[#9BA885]">
+        <p className="text-base text-[#FCFDC7] font-semibold ml-[18px]">
+          {text}
+        </p>
       </div>
       <input
         className={cn(customInput.slideBar, "w-[382px] ml-[12px] mt-[22px]")}
