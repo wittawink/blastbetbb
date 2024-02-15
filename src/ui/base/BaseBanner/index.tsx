84120ba552d5558782241d1b41b27b1c2bf30f1d
popohatari <ajubuwoohoo@gmail.com>
2024-02-16 01:36:17 +0700

    aaaaa

--- src/ui/base/BaseBanner/index.tsx
+++ src/ui/base/BaseBanner/index.tsx
@@ -8,20 +8,46 @@ import { useRouter } from "next/navigation";
 interface BannerProps {
   title: string;
   linkPath: string;
+  text?: string;
+  className?: string;
+  titleColor?: string;
+  subtitleColor?: string;
 }
 
-export default function BaseBanner({ title, linkPath }: BannerProps) {
+export default function BaseBanner({
+  title,
+  linkPath,
+  text,
+  className,
+  titleColor,
+  subtitleColor,
+}: BannerProps) {
   const router = useRouter();
   const handleOnClickBanner = () => {
     router.push(linkPath);
   };
   return (
     <BaseButton
-      className={cn(customInput.bannerCon)}
+      className={cn(customInput.bannerCon, className)}
       onClick={handleOnClickBanner}
     >
-      <p className="text-4xl text-[#404833] font-geomGraphic font-bold mx-9 mt-6 top-0 left-0 absolute">
+      <p
+        className={cn(
+          customInput,
+          titleColor,
+          "text-4xl text-[#404833] font-geomGraphic font-bold mx-9 mt-6 top-0 left-0 absolute whitespace-nowrap"
+        )}
+      >
         {title}
+      </p>
+      <p
+        className={cn(
+          customInput,
+          subtitleColor,
+          "text-xl text-left text-[#404833] font-geomGraphic font-bold mx-9 left-0"
+        )}
+      >
+        {text}
       </p>
     </BaseButton>
   );
--- src/ui/core/HomeDetail/index.tsx
+++ src/ui/core/HomeDetail/index.tsx
@@ -27,6 +27,54 @@ export default function HomeDetail() {
         <BaseBanner title={"Coming soon"} linkPath={"#"}></BaseBanner>
         <BaseBanner title={"Coming soon"} linkPath={"#"}></BaseBanner>
       </div>
+      <BaseGradientView
+        className="h-[240px] flex items-center justify-center mt-8 "
+        fromColor={"from-[#75835D]"}
+        toColor={"to-[#C3D4A5]"}
+        borderColor={"border-[#404833]"}
+      >
+        <div className="text-center font-geomGraphic text-[60px] text-[#11140C] font-semibold">
+          Our Niche!!
+        </div>
+      </BaseGradientView>
+      <div className="grid grid-rows-2 grid-cols-3 gap-4  w-full mt-[50px]">
+        <BaseBanner
+          title={"Fair"}
+          text={"100% of Betting Profits (House-edge) Returned to Users"}
+          linkPath={"#"}
+          className={"!h-[380px] !bg-[#FCFC03]"}
+          titleColor={"!text-black"}
+          subtitleColor={"!text-black"}
+        ></BaseBanner>
+        <BaseBanner
+          title={"Unique"}
+          text={"Featuring PoolTogether, Ranked Matchmaking Bets, and More"}
+          linkPath={"#"}
+          className={"!h-[380px] !bg-[#FCFC03]"}
+          titleColor={"!text-black"}
+          subtitleColor={"!text-black"}
+        ></BaseBanner>
+        <BaseBanner
+          title={"Strongly-Supported"}
+          linkPath={"#"}
+          text={
+            "Powered by the Blast Ecosystem, Encouraging BLAST and USDB Usage for Mutual Benefits"
+          }
+          className={"!h-[380px] !bg-[#FCFC03]"}
+          titleColor={"!text-black"}
+          subtitleColor={"!text-black"}
+        ></BaseBanner>
+      </div>
+      <BaseGradientView
+        className="h-[240px] flex items-center justify-center"
+        fromColor={"from-[#75835D]"}
+        toColor={"to-[#C3D4A5]"}
+        borderColor={"border-[#404833]"}
+      >
+        <div className="text-center font-geomGraphic text-[60px] text-[#11140C] font-semibold">
+          Our Niche!!
+        </div>
+      </BaseGradientView>
     </main>
   );
 }
